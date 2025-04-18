20039a5fffd0fe33c791e5b086703411ac8dc7fe
brndnknn <brndnknn@gmail.com>
2025-04-18 16:15:15 -0500

    added function to run model multiple times to keep track of and average runtimes.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from utils.runner import run_model
+from utils.runner import run_multiple_times
 import time
 
 def main():
     model = "gemma3:1b"
+    #4b-it-q8_0"
     prompt = "Explain the concept of gravity in one paragraph."
+    count = 5
 
     print(f"Running model '{model}'...")
 
     start_time = time.time()
-    output = run_model(prompt, model)
+    run_multiple_times(prompt, model, count)
     elapsed = time.time() - start_time
 
-    print("\n--- Response --- \n")
-    print(output)
-    print(f"\nTime taken: {elapsed:.2f} seconds")
+
+    print(f"\nTotal time taken: {elapsed:.2f} seconds")
 
 if __name__ == "__main__":
     main()
--- utils/runner.py
+++ utils/runner.py
@@ -1,4 +1,22 @@
 import requests
+import time
+
+def run_multiple_times(prompt: str, model: str, count: int):
+    run_times = []
+
+    for i in range(count):
+        print(f"\nRun {i+1}/{count}...")
+        start = time.time()
+        output = run_model(prompt, model)
+        elapsed = time.time() - start
+        run_times.append(elapsed)
+
+        print(f"Response Time: {elapsed:.2f}s")
+    
+    avg_time = sum(run_times) / len(run_times)
+    print(f"\n--- Summary for {model} ---\n")
+    print(f"Average Time: {avg_time:.2f}s over {count} runs")
+
 
 def run_model(prompt: str, model: str) -> str:
     response = requests.post(
